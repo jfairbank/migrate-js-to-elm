296a54849ff8d91dad2e67cbe7f36ad0946b6973
Jeremy Fairbank <elpapapollo@gmail.com>
2017-11-04 16:32:15 -0400

    Add elm loader to prod config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -212,6 +212,14 @@ module.exports = {
             ),
             // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
           },
+          {
+            test: /\.elm$/,
+            exclude: [/elm-stuff/, /node_modules/],
+            use: {
+              loader: 'elm-webpack-loader',
+              options: {},
+            },
+          },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader doesn't use a "test" so it will catch all modules
