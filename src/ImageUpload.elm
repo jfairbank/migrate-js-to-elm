d40a77bd8bc99cdada1aca9d3dcb793813d5560c
Jeremy Fairbank <elpapapollo@gmail.com>
2017-11-07 08:06:08 -0500

    Upload images

--- src/ImageUpload.elm
+++ src/ImageUpload.elm
@@ -1,16 +1,50 @@
-module ImageUpload exposing (main)
+port module ImageUpload exposing (main)
 
-import Html exposing (Html, div, input, label, text)
-import Html.Attributes exposing (class, for, id, multiple, type_)
+import Html exposing (Html, div, img, input, label, li, text, ul)
+import Html.Attributes exposing (class, for, id, multiple, src, type_, width)
+import Html.Events exposing (on)
+import Json.Decode as Decode exposing (succeed)
+
+
+onChange : msg -> Html.Attribute msg
+onChange msg =
+    on "change" (succeed msg)
+
+
+type alias Id =
+    String
+
+
+type alias Image =
+    { id : Id
+    , url : String
+    }
+
+
+port uploadImages : () -> Cmd msg
+
+
+port receiveImages : (List Image -> msg) -> Sub msg
 
 
 type alias Model =
-    ()
+    { images : List Image }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( (), Cmd.none )
+    ( Model [], Cmd.none )
+
+
+viewImage : Image -> Html Msg
+viewImage image =
+    li [ class "image-upload__image" ]
+        [ img
+            [ src image.url
+            , width 400
+            ]
+            []
+        ]
 
 
 view : Model -> Html Msg
@@ -22,23 +56,34 @@ view model =
             [ id "file-upload"
             , type_ "file"
             , multiple True
+            , onChange UploadImages
             ]
             []
+        , ul [ class "image-upload__images" ]
+            (List.map viewImage model.images)
         ]
 
 
 type Msg
-    = NoOp
+    = UploadImages
+    | ReceiveImages (List Image)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        UploadImages ->
+            ( model, uploadImages () )
+
+        ReceiveImages images ->
+            ( { model | images = images }
+            , Cmd.none
+            )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    receiveImages ReceiveImages
 
 
 main : Program Never Model Msg
--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import cuid from 'cuid';
 import Elm from './ImageUpload.elm';
 import './ImageUpload.css';
 
@@ -6,11 +7,47 @@ class ImageUpload extends Component {
   constructor(props) {
     super(props);
 
+    this.readImages = this.readImages.bind(this);
     this.setElmRef = this.setElmRef.bind(this);
   }
 
   componentDidMount() {
     this.elm = Elm.ImageUpload.embed(this.elmRef);
+
+    this.elm.ports.uploadImages.subscribe(this.readImages);
+  }
+
+  componentWillUnmount() {
+    this.elm.ports.uploadImages.unsubscribe(this.readImages);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.elm.ports.receiveImages.send(nextProps.images);
+  }
+
+  readImage(file) {
+    const reader = new FileReader();
+
+    const promise = new Promise((resolve) => {
+      reader.onload = (e) => {
+        resolve({
+          id: cuid(),
+          url: e.target.result,
+        });
+      };
+    });
+
+    reader.readAsDataURL(file);
+
+    return promise;
+  }
+
+  readImages(images) {
+    const element = document.getElementById('file-upload');
+    const files = Array.from(element.files);
+
+    Promise.all(files.map(this.readImage))
+      .then(this.props.onUpload);
   }
 
   setElmRef(node) {
